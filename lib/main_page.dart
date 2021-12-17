2d62812f4cce15f3c6e03cfb2721143af2ec2f38
avk29 <66971874+avk29@users.noreply.github.com>
2021-12-17 19:47:48 +0530

    adding updates

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -3,6 +3,7 @@ import 'package:piano_tiles_2/song_provider.dart';
 import 'line_divider.dart';
 import 'line.dart';
 import 'note.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -16,20 +17,38 @@ class _MainPageState extends State<MainPage>
   List<Note> notes = initNotes();
   late AnimationController animationController;
   int currentNoteIndex = 0;
+  int points = 0;
+  bool hasStarted = false;
+  bool isPlaying = true;
+  final player = AudioCache();
 
   @override
   void initState() {
     super.initState();
     animationController = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 300));
+        vsync: this, duration: const Duration(milliseconds: 200));
     animationController.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        setState(() {
-          ++currentNoteIndex;
-        });
-        animationController.forward(from: 0);
+      if (status == AnimationStatus.completed && isPlaying) {
+        if (notes[currentNoteIndex].state != Notestate.tapped) {
+          //game is over
+          setState(() {
+            isPlaying = false; //this will stop the game
+            notes[currentNoteIndex].state = Notestate.missed;
+          });
+          animationController.reverse().then((_) =>
+              _showFinishDialog()); //this will reverse the last animation (last note)
+        } else if (currentNoteIndex == notes.length - 5) {
+          //song is finished
+          _showFinishDialog();
+        } else {
+          //if the song did not end
+          setState(() => ++currentNoteIndex); //increase the song index
+          animationController.forward(
+              from: 0); //start the animation all over again
+        }
       }
     });
+    // animationController.forward(); //start the animation for the first time
   }
 
   @override
@@ -42,34 +61,48 @@ class _MainPageState extends State<MainPage>
   Widget build(BuildContext context) {
     return Material(
       child: Stack(
-        fit: StackFit.expand,
+        fit: StackFit.passthrough,
         children: [
           Image.asset(
-            'images/background_image.jpg',
+            'assets/images/background_image.jpg',
             fit: BoxFit.cover,
           ),
           Row(
             children: [
-              _drawline(0),
+              _drawLine(0),
               const LineDivider(),
-              _drawline(1),
+              _drawLine(1),
               const LineDivider(),
-              _drawline(2),
+              _drawLine(2),
               const LineDivider(),
-              _drawline(3),
+              _drawLine(3),
               const LineDivider(),
             ],
-          )
+          ),
+          _drawPoints(),
         ],
       ),
     );
   }
 
   void _onTap(Note note) {
-    animationController.forward();
+    bool areAllPreviousTapped = notes
+        .sublist(0, note.orderNo)
+        .every((n) => n.state == Notestate.tapped);
+    if (areAllPreviousTapped) {
+      if (!hasStarted) {
+        setState(() => hasStarted = true);
+        animationController.forward();
+      }
+      _playNote(note);
+      setState(() {
+        note.state = Notestate.tapped;
+        points++;
+      });
+    }
   }
 
-  _drawline(int lineNumber) {
+  _drawLine(int lineNumber) {
     return Expanded(
         child: Line(
       lineNumber: lineNumber,
@@ -77,5 +110,69 @@ class _MainPageState extends State<MainPage>
       onTileTap: _onTap,
       animation: animationController,
     ));
+  }
+
+  _drawPoints() {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 32),
+        child: Text(
+          "$points",
+          style: const TextStyle(
+              color: Colors.red, fontSize: 70, fontFamily: 'Alata'),
+        ),
+      ),
+    );
+  }
+
+  _playNote(Note note) {
+    switch (note.line) {
+      //To play a sound depending on the line number of the Note
+      case 0:
+        player.play('sounds/a.wav');
+        return;
+      case 1:
+        player.play('sounds/c.wav');
+        return;
+      case 2:
+        player.play('sounds/e.wav');
+        return;
+      case 3:
+        player.play('sounds/f.wav');
+        return;
+    }
+  }
+
+  void _restart() {
+    setState(() {
+      hasStarted = false;
+      isPlaying = true;
+      notes = initNotes();
+      points = 0;
+      currentNoteIndex = 0;
+    });
+    animationController.reset();
+  }
+
+  void _showFinishDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Game Over! \nScore: $points"),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text(
+                "RESTART",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        );
+      },
+    ).then(
+        (_) => _restart()); //restart the game when the popup dialogue is closed
   }
 }
